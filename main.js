830459e60d9d097d17f03c7fb37bd209111cb927
LiorDavid1996 <liordavid356@gmail.com>
2022-05-23 16:31:53 +0300

    commit

---
+++ main.js
@@ -0,0 +1,217 @@
+   // for(i=0 ;i<5 ;i++){
+        //     console.log(8)
+        // }
+        // var num1=prompt()
+        // var num2=prompt()
+        // console.log(num1,num2)
+        // var a=+prompt()
+        // if(a==5){
+        //     document.write("true")
+
+        // }
+        // var nul=prompt()*prompt()*prompt()
+        // document.write(nul)
+        // var first=prompt()
+        // var second=prompt()
+        // var result=first%second
+        // document.write(result)
+        // var a=5
+        // a++
+        // document.write(a)
+        // var a=6
+        // a=a*3
+        // document.write(a)
+        // var a = prompt()
+        // var s=2*(3.14)*a
+        // var v=a^2*(3.14)
+        // console.log(s,v)
+
+        //     var a=prompt("player name")
+        //     var ageA=prompt("player age")
+        //     var b=prompt("player name")
+        //     var ageB=prompt("player age")
+        //     var c=prompt("player name")
+        //     var ageC=prompt("player age")
+        //     var avg=(ageA+ageB+ageC)
+        //    console.log(a,b,c)
+        //     console.log((avg)/3)
+        // var names = ["lior", "aviel", "ran", "batel", "shimon"];
+        // console.log(names);
+        // console.log(names[2])
+
+        // for (var i = 0; i < names.length; i++) {
+        //     console.log(names[i])  }
+
+        //     var num = [102,111,187,152 ,797,866,367]
+        //     console.log(num)
+        //     console.log(num[3])
+        //     for(i=0; i<num.length;i++){
+        //         console.log(num[i])
+        //     }
+        // var num=[35,46,28,48,12,17,9,36,42,21]
+        // console.log(num)
+        // for(i=0 ; i<num.length; i++){
+        //     if(num[i]%2==0){
+        //     console.log(num[i])}
+        // // }
+        // var myArray = ["hello" ,2 ,"world","jecob",6,7,"bye",98]
+        // for(i=0; i<myArray.length; i++){ 
+        //     if(myArray[i]*1 !=myArray[i] ){
+        //         console.log(myArray[i])
+        //     } }
+        // var someArray = [];
+        // someArray.push(9);
+        // console.log(someArray);
+
+        // var someArrai = [];
+        // for(i=0 ; i<10 ; i++){
+        // someArrai.push(i)
+
+        // }
+        // console.log(someArrai)
+        // var someArray=[5,3,7,8,11]
+        // var sum=0
+        // for(i=0;i<someArray.length; i++){
+        //     sum+=someArray[i]   
+        // }
+        // console.log(sum)
+        // console.log(sum/someArray.length)
+        // var names=["lior","david","aviel","bat-el","nati"]
+        // for(i=0; i<names.length;i++){
+        //     console.log(names[i])
+        // }
+        // function arrayNum (array, num){
+        //     for(i=0; i<array.length ;i++){
+        //         if(array[i]==num){
+        //             return true
+        //         }
+        //     }
+        //     return false 
+        // }
+        //     console.log(arrayNum([2,4,6,9],6))
+
+
+        //         function numEven(array){
+        //            var count=0
+        //         for(i=0 ;i<array.length;i++){
+        //             if(array[i]%2 ==0){
+        //                 count++;
+        //              }
+        //         }
+        //         return count
+        //     }
+        //    console.log (numEven([1,2,3,4,5,6,7,8,9,12,15,16]))
+
+        // function renum(array,arrai){
+        //     if(arrai.length==array.length){
+        //     return true
+        // }
+        // return false
+        // }
+        // console.log(renum([1,2,3,4],[3,5]))
+        // function num(even){ 
+        //     var max=even[0]
+        //     for(i=0; i<even.length; i++){
+        //         if(even[i]>max){
+        //             max=even[i]
+
+        //         }
+
+        //     }
+        //     return max
+        // }
+        // console.log(num([3,5,323,7,5,3,8]))
+
+        //     function minNum(numev){
+        //         var moon=numev[0];
+        //         var minindex=0;
+        //         for(i=0; i<numev.length;i++){
+        //             if(moon>numev[i]){
+        //              moon  = numev[i]
+        //              minindex =i;
+        //             }
+
+        //         }
+        //         console.log(minindex)
+        //     }
+        // minNum([1,33,45,44,32,6,5,0]);
+
+        // function sumNume(num){
+        //     var sum=0
+        //     for(i=0;i<num.length;i++){
+        //         sum+=num[i]
+
+        //     }
+        //     console.log(sum);
+
+        // }
+        // sumNume([2,4,55,6,7,3]);
+
+        // function minmux(num2){
+        //     var max=0;
+        //     var min=num2[0]
+
+
+        //     for(i=0;i<num2.length;i++){
+        //         if(num2[i]>max){
+        //             max=num2[i]
+        //         }
+        //         if(num2[i]<min){
+        //             min=num2[i]
+        //         }
+
+        //     }
+        //     console.log(max*min)
+
+        // }
+        // minmux([3,2,1,8,6,4,12]);
+
+            // function evenh(x){
+            //    var max=true;
+            //     var indexNum=0
+            //     var count =0;
+            //     while(max ==true&&count<x.length){
+            //         if(x[count]%2==0){
+            //             max =x[count];
+            //         }
+            //     }
+            //     for(i=0; i<x.length; i++){
+            //         if(x[i]%2==0 ){
+                        
+            //       if(x[i]>max)
+            //       max=x[i]          
+            //     indexNum=i
+                
+            //         }
+            //     }
+
+                
+            //     console.log(max,i)
+            // }
+            // evenh([12,3,5,122,65,3,7])
+
+        // function sumNum(y){
+        //     var sum=0
+        //     for(i=0; i<y.length; i++){
+        //         if(y[i]%2==1)
+        //         sum+=y[i]
+        //     }
+        //     console.log(sum)
+        // }sumNum([12,37,43,64,35,76,54,245,76])\
+
+    //     function end(x, y) {
+    //         x.push(y)
+    //         return x;
+    //         console.log(x)
+
+    //     }
+    //   end([2, 43, 5, 32], 4)
+
+    // function start(d,s){
+    //     d.unshift (s)
+    //     return d
+        
+    // }
+    // console.log(["lior@gmail","batel@efcv",],"yarden@ggg");
+
+    // date
